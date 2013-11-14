70b6e0feaf0565428eeab48994bae52078d867af
David Conner <dconner.pro@gmail.com>
2013-11-13 23:43:29 -0700

    added devise json routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Railsapp::Application.routes.draw do
   scope :api do
 
     devise_for :users
+    devise_scope :user do
+      post 'login' => 'sessions#create', :as => 'login'
+      post 'logout' => 'sessions#destroy', :as => 'logout'
+      get 'current_user' => 'sessions#show_current_user', :as => 'show_current_user'
+    end
+
     resources :users
     root to: "home#index"
 
