20ee19546e724034304dff792f5e2e199c7d542a
David Conner <dconner.pro@gmail.com>
2013-11-15 18:09:05 -0700

    Devise: configured parameters to permit on signup/signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  
   after_filter :set_csrf_cookie_for_ng
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password) }
   end
 end
