383320a1345b5289ea66173950e451ca11bd7c69
David Conner <dconner.pro@gmail.com>
2013-11-15 18:11:37 -0700

    User: added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, #TODO: :confirmable
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+  validates :username, length: { in: 4..20 }
+
+  #TODO: login with email or username
+  #TODO: add remember_me
 end
