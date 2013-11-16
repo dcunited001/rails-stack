b0ea01fd2793fcc5676b01436be983dd226bab77
David Conner <dconner.pro@gmail.com>
2013-11-15 18:11:12 -0700

    Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  #before_filter :authenticate_user!
+  respond_to :json
+
   def index
+    respond_with User.all
   end
 
   def show
+    respond_with User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
