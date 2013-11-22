86cf094cd6b88abe7c1aafa5454ab38062b7fa51
David Conner <dconner.pro@gmail.com>
2013-11-22 16:20:55 -0700

    minitest: clean up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'minitest/pride'
 
-module MiniTestSpecRails
-  class TestCase < MiniTest::Spec
-
-  end
-end
-
 require 'rails/test_help'  
 class ActiveSupport::TestCase
 
