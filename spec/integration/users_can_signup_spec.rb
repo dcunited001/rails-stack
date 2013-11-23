5614f790766eb8a41f0567ba4444a68ddb1d59f5
David Conner <dconner.pro@gmail.com>
2013-11-23 16:52:28 -0700

    integration: added basic test for signing up and logging in
    * still need to add a few things, see comments

--- spec/integration/users_can_signup_spec.rb
+++ spec/integration/users_can_signup_spec.rb
@@ -1,10 +1,44 @@
 require 'spec_helper_integration'
 
 class ApplicationTest < ActionDispatch::IntegrationTest
-  describe "Users can signup" do 
-    it 'has a signup modal' do
+  describe "Users can signup and login", js: true do 
+    let(:email) { "me@me.com" }
+    let(:username) { "me_username" }
+    let(:password) { "PassWord!" }
+
+    before do
       visit '/'
-      save_and_open_page
+    end
+
+    it 'using the modal forms' do
+      within '.navbar' do
+        click_button "Sign Up"
+      end
+
+      within 'form#signup-modal' do
+        fill_in "Email", with: email
+        fill_in "Username", with: username
+        fill_in "Password", with: password
+        fill_in "Confirm", with: password
+        click_button "Sign Up"
+      end
+
+      # TODO: assert modal closed
+      # TODO: add database cleaner to fix validation issue
+      # TODO: assert confirmation email queued
+      # TODO: clicking confirmation link activates user
+
+      within '.navbar' do
+        click_button "Log In"
+      end
+
+      within 'form#login-modal' do
+        fill_in "Email", with: email
+        fill_in "Password", with: password
+        click_button "Log In"
+      end
+
+      # TODO: assertions
     end
   end
 end
