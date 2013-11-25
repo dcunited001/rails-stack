c1193796af637612db49bb0a2599195a3fe6a708
David Conner <dconner.pro@gmail.com>
2013-11-24 21:34:04 -0700

    UsersController: validating user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
