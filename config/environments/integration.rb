3a24e63a70376b818ae59c3ad3438b79cec6649c
David Conner <dconner.pro@gmail.com>
2014-02-27 21:20:47 -0500

    added ng_client_url to integration config

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -1,4 +1,5 @@
 Railsapp::Application.configure do
+  config.ng_client_url = "http://localhost:3000"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
